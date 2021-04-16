e561a4e7bbc7b90fecbd2fa9738ab8f001be8330
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-04-16 15:38:56 -0400

    css y js

---
+++ script.js
@@ -0,0 +1,7 @@
+//La tarea a realizar tiene que estar hecha con HTML, CSS y JS.
+//Los puntos a cubrir son los siguientes:
+//Cumplir con la estructura que se manda.
+//Usar tipos de letras de su preferencia.
+//La barra verde que ven en el ejemplo quiero que desaparezca cuando la página web se visualice desde una medida de 400px y ajustar la otra de color gris a un 100% de la pantalla.
+//Cuando le de clic al botón enviar quiero que mande una alerta con javascript  mandando un mensaje que gusten.
+//Subir su tarea a github.
